962a52b75f6e8e4af7790352ed01d6674e64d881
Zaur Zhane <zhanezaur89@gmail.com>
2023-07-13 13:31:48 +0300

    finalization of the task Calculator

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -18,16 +18,7 @@ class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> {
-        try {
-            if (y == 0)
-                throw new ArithmeticException("Ошибка: деление на ноль.");
-            return x / y;
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
-            return 0;
-        }
-    };
+    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? x / Integer.MAX_VALUE: x / y;
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
