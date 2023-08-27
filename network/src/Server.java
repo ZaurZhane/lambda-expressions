8c2da02dd54872d1907b07baa9c70daf86fae22e
Zaur Zhane <zhanezaur89@gmail.com>
2023-08-27 20:37:07 +0300

    OSI correction

--- network/src/Server.java
+++ network/src/Server.java
@@ -7,21 +7,22 @@ import java.net.Socket;
 
 public class Server {
 
-    public static void main(String [ ] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
         int port = 8086;
 
-        ServerSocket serverSocket = new ServerSocket(port);
-        Socket clientSocket = serverSocket.accept(); // ждем подключения
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket clientSocket = serverSocket.accept(); // ждем подключения
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-        System.out.println("New connection accepted");
+            System.out.println("New connection accepted");
 
-        final String name = in.readLine();
+            final String name = in.readLine();
 
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
 
+        }
     }
 
 
