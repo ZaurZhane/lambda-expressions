fdf2572ef5d48aed4a800c824110d4ada82ec9f2
Zaur Zhane <zhanezaur89@gmail.com>
2023-10-21 09:12:03 +0300

    Stub and tests for add

--- TestDrivenDevelopment/src/main/java/PhoneBook.java
+++ TestDrivenDevelopment/src/main/java/PhoneBook.java
@@ -1,2 +1,16 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PhoneBook {
+
+    Map<String, String> list = new HashMap<>();
+
+    public int add(String name, String phone) {
+
+       return 0;
+
+    }
+
+
 }
--- TestDrivenDevelopment/src/test/java/PhoneBookTest.java
+++ TestDrivenDevelopment/src/test/java/PhoneBookTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+
+    @Test
+    public void addTest() {
+
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Жане Заур Азаматович", "+79615285387");
+        int numberRecords = phoneBook.add("Жане Заур Азаматович", "+79603255588");
+
+        Assertions.assertEquals(1, numberRecords);
+
+    }
+
 }
