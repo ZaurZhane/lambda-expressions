0ee1c60ad730efc7adef8ea009edacdb07f72e92
zaur <zhanezaur89@gmail.com>
2024-01-24 07:52:46 +0300

    CRUD update

--- Servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ Servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -10,7 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
     private PostController controller;
-
+    private static final String GET_METHOD = "GET";
+    private static final String POST_METHOD = "POST";
+    private static final String DELETE_METHOD = "DELETE";
+    private static final String PATH = "/api/posts";
+    private static final String PATH_WITH_PARAMETER = "/api/posts/\\d+";
     @Override
     public void init() {
         final var repository = new PostRepository();
@@ -25,21 +29,21 @@ public class MainServlet extends HttpServlet {
             final var path = req.getRequestURI();
             final var method = req.getMethod();
             // primitive routing
-            if (method.equals("GET") && path.equals("/api/posts")) {
+            if (method.equals(GET_METHOD) && path.equals(PATH)) {
                 controller.all(resp);
                 return;
             }
-            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+            if (method.equals(GET_METHOD) && path.matches(PATH_WITH_PARAMETER)) {
                 // easy way
                 final var id = getId(path);
                 controller.getById(id, resp);
                 return;
             }
-            if (method.equals("POST") && path.equals("/api/posts")) {
+            if (method.equals(POST_METHOD) && path.equals(PATH)) {
                 controller.save(req.getReader(), resp);
                 return;
             }
-            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+            if (method.equals(DELETE_METHOD) && path.matches(PATH_WITH_PARAMETER)) {
                 // easy way
                 final var id = getId(path);
                 controller.removeById(id, resp);
